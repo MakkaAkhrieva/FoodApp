55ae5def67f8af64aaa736bf509168b7f7ccd0fd
Makka Akhrieva <makka.akhrieva00@mail.ru>
2022-01-23 02:20:46 +0300

    fetch request ap implementation

--- js/script.js
+++ js/script.js
@@ -241,10 +241,6 @@ window.addEventListener("DOMContentLoaded",()=>{
             
             form.insertAdjacentElement('afterend',statusMessage);
 
-            const request= new XMLHttpRequest();
-            request.open('POST','server.php');
-
-            /* request.setRequestHeader('Content-type','multipart/form-data'); */
             const formData = new FormData(form);
 
             const object ={};
@@ -253,22 +249,23 @@ window.addEventListener("DOMContentLoaded",()=>{
                 object[key]=value;
             });
 
-            const json =JSON.stringify(object);
-
             //form - та форма откуда нудно взять данные
 
-            request.send(json);
-            request.addEventListener('load',()=>{
-                if(request.status===200){
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-        
-                }else{
-                    showThanksModal(message.failure);
-
-                }
+            fetch('server.php',{
+                method:"POST",
+                headers:{
+                    'Content-type':'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data=>data.text())
+            .then(data=>{
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(()=>{
+                showThanksModal(message.failure);
+            }).finally(()=>{
+                form.reset();
             })
 
 
@@ -298,4 +295,5 @@ window.addEventListener("DOMContentLoaded",()=>{
         }, 4000);
     }
    
+
 });
